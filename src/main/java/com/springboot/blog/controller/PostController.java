ff669715344d840323fd84133538827d366521f6
parvez-ahmad <parveziahmad09@gmail.com>
2022-06-06 08:36:18 +0530

    second commit

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -2,7 +2,9 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,29 +21,41 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
-    return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    //Code for ResponseENtity for list<Object>
+    /*@GetMapping
+    public ResponseEntity<Object> getAllPosts(@RequestParam(value="pageNo",defaultValue = "0",required = false) int pageNo,
+                                    @RequestParam(value="pageSize",defaultValue = "10",required = false) int pageSize
+                                     ) {
+        PostResponse allPost = postService.getAllPost(pageNo, pageSize);
+        return new ResponseEntity<>(allPost,HttpStatus.OK);
+    }*/
     @GetMapping
-    public List<PostDto> getAllPosts(){
-        return postService.getAllPost();
-    }
+    public PostResponse getAllPosts(@RequestParam(value="pageNo",defaultValue = AppConstant.default_page_Number,required = false) int pageNo,
+                                              @RequestParam(value="pageSize",defaultValue = AppConstant.default_page_size,required = false) int pageSize,
+                                    @RequestParam(value="sortBy",defaultValue = AppConstant.default_sort_by,required = false) String sortBy,
+                                    @RequestParam(value = "sortDir",defaultValue = AppConstant.default_sort_direction,required = false) String sortDir
+    ) {
 
+        return postService.getAllPost(pageNo, pageSize,sortBy,sortDir);
+    }
     @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostbyId(@PathVariable Long id){
+    public ResponseEntity<PostDto> getPostbyId(@PathVariable Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
-       //return new ResponseEntity<>(postService.getPostById(id),HttpStatus.OK);
+        //return new ResponseEntity<>(postService.getPostById(id),HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatepost(@RequestBody PostDto postDto,@PathVariable Long id){
-        return new ResponseEntity<>(postService.updatePost(postDto,id),HttpStatus.OK);
+    public ResponseEntity<PostDto> updatepost(@RequestBody PostDto postDto, @PathVariable Long id) {
+        return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletePostById(@PathVariable Long id){
-      postService.deletePostByid(id);
-      return new ResponseEntity<>("post is deleted for this id :"+id,HttpStatus.OK);
+    public ResponseEntity<?> deletePostById(@PathVariable Long id) {
+        postService.deletePostByid(id);
+        return new ResponseEntity<>("post is deleted for this id :" + id, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/springboot/blog/utils/AppConstant.java
@@ -0,0 +1,8 @@
+package com.springboot.blog.utils;
+
+public class AppConstant {
+    public static final String default_page_Number="0";
+    public static final String default_page_size="10";
+    public static final String default_sort_by="id";
+    public static final String default_sort_direction="asc";
+}
